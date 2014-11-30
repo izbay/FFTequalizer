75b2bd8979c3f415b2ef54a2f6b0f8f0077130f2
izbay <jacobmartinek@yahoo.com>
2014-11-29 19:03:31 -0500

    Rudimentary FFT implemented

--- FFT_Project/Form1.Designer.cs
+++ FFT_Project/Form1.Designer.cs
@@ -29,12 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -77,21 +82,53 @@
             this.toolStripButton3.Name = "toolStripButton3";
             this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton3.Text = "toolStripButton3";
-            this.toolStripButton3.Click += new System.EventHandler(this.pauseSong);
+            this.toolStripButton3.Click += new System.EventHandler(this.stopSong);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 28);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(624, 300);
+            this.textBox1.Size = new System.Drawing.Size(624, 20);
             this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.LineWidth = 0;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.AxisY.LabelStyle.Enabled = false;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisY.LineWidth = 0;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.Maximum = 4000D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 54);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(624, 274);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(648, 340);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.toolStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -99,6 +136,7 @@
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +149,7 @@
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- FFT_Project/Form1.cs
+++ FFT_Project/Form1.cs
@@ -14,6 +14,10 @@ namespace FFT_Project
 {
     public partial class Form1 : Form
     {
+        private static System.Timers.Timer aTimer;
+        private static int counter = 0;
+        private static double[] doubleArray;
+
         private SoundPlayer songPlayer;
 
         public Form1()
@@ -34,17 +38,19 @@ namespace FFT_Project
 
                     textBox1.Text = openedFile.FileName;
 
-                    byte[] bytes = File.ReadAllBytes(openedFile.FileName);
+                    int fileHeaderSize = 44;
+                    byte[] bytes = File.ReadAllBytes(openedFile.FileName).Skip(fileHeaderSize).ToArray();
 
-                    String byteString = "";
+                    List<double> dList = new List<double>();
+                    bytes.ToList<byte>().ForEach(b => dList.Add(Convert.ToDouble(b)));
+                    doubleArray = dList.ToArray<double>();
+
+                    // Create a timer with a 1ms interval.
+                    aTimer = new System.Timers.Timer(75);
+                    // Hook up the Elapsed event for the timer. 
+                    aTimer.Elapsed += OnTimed;
+           
                     
-                    for (int i = 0; i < 4096; i++)
-                    {
-                        byteString += bytes[i];
-                        byteString += " ";
-                    }
-                    
-                    textBox1.Text = /*bytes.Length.ToString();*/ byteString;
                 }
 
                 catch (Exception ex)
@@ -55,12 +61,48 @@ namespace FFT_Project
 
         }
 
+        void OnTimed(object sender, EventArgs e)
+        {
+            int sampleSize = 32;
+            if (counter > doubleArray.Length / sampleSize)
+            {
+                this.Invoke((MethodInvoker)delegate { chart1.Series["Series1"].Points.Clear(); });
+                aTimer.Stop();
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)delegate { doFFT(doubleArray.Skip(counter * sampleSize).Take(sampleSize).ToArray()); });
+                counter += 475; // Skip ahead so we're not sampling concentrated areas repeatedly.
+            }
+        }
+
+        private void doFFT(double[] data)
+        {
+            //String FFTString = "";
+
+            FFT(data);
+
+            chart1.Series["Series1"].Points.Clear();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                //FFTString += data[i];
+                //FFTString += Environment.NewLine;
+
+                chart1.Series["Series1"].Points.AddXY(i + 1, data[i]);
+            }
+
+            //textBox2.Text = FFTString;
+        }
+
         private void playSong(object sender, EventArgs e)
         {
 
             try
             {
+                counter = 0;
                 songPlayer.Play();
+                aTimer.Start();
             }
 
             catch (Exception ex)
@@ -70,9 +112,97 @@ namespace FFT_Project
              
         }
 
-        private void pauseSong(object sender, EventArgs e)
+        private void stopSong(object sender, EventArgs e)
         {
+            aTimer.Stop();
+            chart1.Series["Series1"].Points.Clear();
             songPlayer.Stop();
+        }
+
+        /** 
+         * Code below taken from http://www.lomont.org/Software/Misc/FFT/LomontFFT.html for reference.
+         * We will have to study/rewrite it as necessary, but this at least gets us moving.
+        **/
+
+        public void FFT(double[] data)
+        {
+            Reverse(data);                                                    
+
+            // do transform: so single point transforms, then doubles, etc.
+            int n = data.Length / 2, mmax = 1;
+            while (n > mmax)
+            {
+                var istep = 2 * mmax;
+                var theta = Math.PI / mmax;
+                double wr = 1, wi = 0;
+                var wpr = Math.Cos(theta);
+                var wpi = Math.Sin(theta);
+                for (var m = 0; m < istep; m += 2)
+                {
+                    for (var k = m; k < 2 * n; k += 2 * istep)
+                    {
+                        var j = k + istep;
+                        var tempr = wr * data[j] - wi * data[j + 1];
+                        var tempi = wi * data[j] + wr * data[j + 1];
+                        data[j] = data[k] - tempr;
+                        data[j + 1] = data[k + 1] - tempi;
+                        data[k] = data[k] + tempr;
+                        data[k + 1] = data[k + 1] + tempi;
+                    }
+                    var t = wr; // trig recurrence                                                               
+                    wr = wr * wpr - wi * wpi;
+                    wi = wi * wpr + t * wpi;
+                }
+                mmax = istep;
+            }
+        }
+
+        static void Reverse(double[] data)
+        {
+            // bit reverse the indices. This is exercise 5 in section                                            
+            // 7.2.1.1 of Knuth's TAOCP the idea is a binary counter                                             
+            // in k and one with bits reversed in j                                                              
+            int j = 0, k = 0, n = data.Length/2; // Knuth R1: initialize                                                            
+            var top = n / 2;  // this is Knuth's 2^(n-1)                                                         
+            while (true)
+            {
+                // Knuth R2: swap - swap j+1 and k+2^(n-1), 2 entries each                                       
+                var t = data[j + 2];
+                data[j + 2] = data[k + n];
+                data[k + n] = t;
+                t = data[j + 3];
+                data[j + 3] = data[k + n + 1];
+                data[k + n + 1] = t;
+                if (j > k)
+                { // swap two more                                                                               
+                    // j and k                                                                                   
+                    t = data[j];
+                    data[j] = data[k];
+                    data[k] = t;
+                    t = data[j + 1];
+                    data[j + 1] = data[k + 1];
+                    data[k + 1] = t;
+                    // j + top + 1 and k+top + 1                                                                 
+                    t = data[j + n + 2];
+                    data[j + n + 2] = data[k + n + 2];
+                    data[k + n + 2] = t;
+                    t = data[j + n + 3];
+                    data[j + n + 3] = data[k + n + 3];
+                    data[k + n + 3] = t;
+                }
+                // Knuth R3: advance k                                                                           
+                k += 4;
+                if (k >= n)
+                    break;
+                // Knuth R4: advance j                                                                           
+                var h = top;
+                while (j >= h)
+                {
+                    j -= h;
+                    h /= 2;
+                }
+                j += h;
+            } // bit reverse loop                                                                                
         }
     }
 }
